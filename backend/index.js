ccdf2870e00eb14867c5e4a03ff45244eb0509a7
Kayo Renato <kayorenatocontato@gmail.com>
2020-01-14 20:40:58 -0300

    created route DELETE

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,23 @@ const express = require('express'); //library available to create routes for app
 
 const app = express(); //function for acess routes
 
+// HTTP METHODS: GET, PUT, POST, DELETE
 
-// when the user to access route 'localhost:3333/' a function will be executed
-app.get('/', (req, res) => {
+// PARAMETER TYPES:
+
+// Query params - req.query(filter, sort, pagination, ...) - GET
+// Routes params - req.params(identify a resource on the modification or removal)
+
+
+//created first route -  when the user to access 'localhost:3333/user' the route will execute a function
+app.get  ('/user', (req, res) => {
+  console.log(req.query);
+  return  res.json({ message: 'Hello Dev'}); // respond of static form with object Json, but can be other types (image, text, ...). require nodemon library for respond dynamically
+} ); 
+
+//created second route - user removed through informed identification params(./:id)
+app.delete('/user/:id', (req, res) => {
+  console.log(req.params);
   return  res.json({ message: 'Hello Dev'}); // respond of static form with object Json, but can be other types (image, text, ...). require nodemon library for respond dynamically
 } ); 
 
