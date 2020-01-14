c73e6154814ae2b5b0d530e5e2bf155376656946
Kayo Renato <kayorenatocontato@gmail.com>
2020-01-14 18:40:26 -0300

    add nodemon library for respond dynamically the request

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
-const express = require('express'); //libray to create routes for app
+const express = require('express'); //library available to create routes for app
 
-const app = express();
+const app = express(); //function for acess routes
+
+
+// when the user to access route 'localhost:3333/' a function will be executed
+app.get('/', (req, res) => {
+  return  res.json({ message: 'Hello OmniStack'}); // respond of static form with object Json, but can be other types (image, text, ...). require nodemon library for respond dynamically
+} ); 
+
+
+app.listen(3333); // acess to local server, localhost on port 3333
