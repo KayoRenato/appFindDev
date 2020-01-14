55ba4c3afa5645ab290124fdac22e3054ffdb555
Kayo Renato <kayorenatocontato@gmail.com>
2020-01-14 19:01:48 -0300

    add nodemon library for respond dynamically the request

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,9 @@
 const express = require('express'); //libray to create routes for app
 
 const app = express();
+
+app.get('/', (req, res) => {
+  return res.json({message: 'Hello World'}); //
+});
+
+app.listen(3333); //
