af1c2c4a1834fdba916614bdbdf835224a9c6ac3
Kayo Renato <kayorenatocontato@gmail.com>
2020-01-15 00:09:44 -0300

    Restructuring folders and creating the routes.js file

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express'); //library available to create routes for app
+const mongoose = require('mongoose'); //library enables to communicate the app with mongodb
+const routes = require('./routes'); //import routes from file routes.js
+
+const app = express(); //function for acess routes
+
+
+
+// MongoDB Atlas (noSQL Cloud)
+// check user, password and change default database name 'test'
+mongoose.connect('mongodb+srv://kayo:m0ng0_081187@cluster0-glex1.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+});
+
+app.use(express.json()); //  The requests 'PUT' and 'POST' respond through json format and as the application native not understand format json, so is necessary to registry this format on request express. Must come before routes
+app.use(routes); // providing all registered routes to the app
+
+
+app.listen(3333); // acess to local server, localhost on port 3333
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,6 @@
-const express = require('express'); //library available to create routes for app
-const mongoose = require('mongoose'); //library enables to communicate the app with mongodb
-const app = express(); //function for acess routes
+const {Router} = require('express'); //library available to create routes for app
 
-app.use(express.json()); //  The requests 'PUT' and 'POST' respond through json format and as the application native not understand format json, so is necessary to registry this format on request express.
-
-mongoose.connect('mongodb+srv://kayo:m0ng0_081187@cluster0-glex1.mongodb.net/week10?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true 
-});
-// check user, password and change default database name 'test'
+const routes = Router();
 
 // HTTP METHODS: GET, PUT, POST, DELETE
 
@@ -19,24 +11,22 @@ mongoose.connect('mongodb+srv://kayo:m0ng0_081187@cluster0-glex1.mongodb.net/wee
 // Body - req.body, (data to create or modify a registry) - PUT OR POST
 
 //created first route -  when the user to access 'localhost:3333/user' the route will execute a function
-app.get  ('/users', (req, res) => {
+routes.get  ('/users', (req, res) => {
   console.log(req.query);
   return  res.json({ message: 'Hello Dev'}); // respond of static form with object Json, but can be other types (image, text, ...). require nodemon library for respond dynamically
 } ); 
 
 //created second route - user removed through informed identification params(./:id)
-app.delete('/users/:id', (req, res) => {
+routes.delete('/users/:id', (req, res) => {
   console.log(req.params);
   return  res.json({ message: 'Hello Dev'}); // respond of static form with object Json, but can be other types (image, text, ...). require nodemon library for respond dynamically
 } ); 
 
 // third route created - to create an user passing your information through json in body
-app.post('/users', (req,res) => {
+routes.post('/users', (req,res) => {
   console.log(req.body);
   return  res.json({ message: 'Hello Dev'});
 });
 
-// MongoDB Atlas (noSQL Cloud)
 
-
-app.listen(3333); // acess to local server, localhost on port 3333
+module.exports = routes; //provide routes por other files
