a264f9a7643b2868bc9a44c854b06ec9a37f4b65
Kayo Renato <kayorenatocontato@gmail.com>
2020-01-15 05:29:06 -0300

    check unique username

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -6,31 +6,38 @@ module.exports = {
   async store(req,res){
     const {github_username, techs, latitude, longitude} = req.body;
   
-    // request github API data. Must pass 'wait' flag due to delayed response
-    const apiRes = await axios.get(`https://api.github.com/users/${github_username}`)
-  
-    // destruction method (name = login) - if name equals false, default is login
-    const { name = login, avatar_url, bio } = apiRes.data;
-  
-    // the techs on Dev.js. are an array of strings, so you must split the string when showing ',' (.split) and removing empty space (.map and .trim)
-    const techsArray = techs.split(',').map(item => item.trim());
-  
-    // assigning the request latitudes and longitudes to the database schema
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude],
-    };
-  
-    // registration create in mongoose database as per schema of Dev.js file registered in models folder
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location,
-    });
-  
+    //verify existence of username in database
+    let dev = await Dev.findOne({github_username}); // ??? WHY SEARCH DATA IN 'Dev.js' ???
+
+    if (!dev){
+
+      // request github API data. Must pass 'wait' flag due to delayed response
+      const apiRes = await axios.get(`https://api.github.com/users/${github_username}`)
+      
+      // destruction method (name = login) - if name equals false, default is login
+      const { name = login, avatar_url, bio } = apiRes.data;
+      
+      // the techs on Dev.js. are an array of strings, so you must split the string when showing ',' (.split) and removing empty space (.map and .trim)
+      const techsArray = techs.split(',').map(item => item.trim());
+      
+      // assigning the request latitudes and longitudes to the database schema
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      };
+      
+      // registration create in mongoose database as per schema of Dev.js file registered in models folder
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location,
+      });
+      
+    }
+
     return  res.json(dev);
   }
 }
