ea40b8e20d4523a7056c785d489452b2620c6bac
Kayo Renato <kayorenatocontato@gmail.com>
2020-01-14 23:38:12 -0300

    Install library mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express'); //library available to create routes for app
-
+const mongoose = require('mongoose'); //library enables to communicate the app with mongodb
 const app = express(); //function for acess routes
 
 app.use(express.json()); //  The requests 'PUT' and 'POST' respond through json format and as the application native not understand format json, so is necessary to registry this format on request express.
+
+mongoose.connect('mongodb+srv://kayo:m0ng0_081187@cluster0-glex1.mongodb.net/week10?retryWrites=true&w=majority');
+// check user, password and change default database name 'test'
 
 // HTTP METHODS: GET, PUT, POST, DELETE
 
@@ -11,7 +14,6 @@ app.use(express.json()); //  The requests 'PUT' and 'POST' respond through json 
 // Query params - req.query, (filter, sort, pagination, ...) - GET
 // Routes params - req.params, (identify a resource on the modification or removal) - DELETE
 // Body - req.body, (data to create or modify a registry) - PUT OR POST
-
 
 //created first route -  when the user to access 'localhost:3333/user' the route will execute a function
 app.get  ('/users', (req, res) => {
@@ -30,6 +32,8 @@ app.post('/users', (req,res) => {
   console.log(req.body);
   return  res.json({ message: 'Hello Dev'});
 });
+
+// MongoDB Atlas (noSQL Cloud)
 
 
 app.listen(3333); // acess to local server, localhost on port 3333
