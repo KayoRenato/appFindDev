3e6aa626f3e04e2fc687b563c4c9c155703b953d
Kayo Renato <kayorenatocontato@gmail.com>
2020-01-16 04:29:08 -0300

    conecting with front-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express'); //library available to create routes for app
 const mongoose = require('mongoose'); //library enables to communicate the app with mongodb
+const cors = require('cors');
 const routes = require('./routes'); //import routes from file routes.js
 
 const app = express(); //function for acess routes
@@ -14,6 +15,7 @@ mongoose.connect('mongodb+srv://kayo:m0ng0_081187@cluster0-glex1.mongodb.net/wee
   useCreateIndex: true,
 });
 
+app.use(cors()); //permit externe access API, {origin: 'http:/localhost:3000' } restrited local use code
 app.use(express.json()); //  The requests 'PUT' and 'POST' respond through json format and as the application native not understand format json, so is necessary to registry this format on request express. Must come before routes
 app.use(routes); // providing all registered routes to the app
 
